d520c5de90d448af13f322ebf197aacb809a3e57
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-03-27 00:50:13 +0530

    Minor changes to singup/signin layout

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 
 function AuthLayout(){
-    return <div className="flex h-screen w-screen bg-red-300">
-        <div className="flex justify-center items-center p-8 w-full">
-            <div className="p-4">TrendHive</div>
-            <Outlet/>
+    return <div className="flex h-screen w-screen bg-black justify-center items-center">
+        <div className="flex w-full max-w-md h-[500px] bg-white rounded-4xl shadow-lg divide-x divide-gray-300">
+            <div className="w-1/2 h-full text-4xl flex justify-center items-center bg-gray-200">TrendHive</div>
+            <div className="w-1/2 flex justify-center items-center bg-white"><Outlet/></div>
         </div>
     </div>
 }
